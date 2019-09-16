9bebf9eb930f8f144f698d34213e03f2992d33de
rooneyrulz <roony0782045336@gmail.com>
2019-09-16 21:42:56 +0530

    setup auth stuffs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,53 @@
-export const authenticateUser = (req, res, next) =>
-  res.status(200).send('Authenticate user..');
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { validationResult } from 'express-validator';
 
-export const getAuthUser = (req, res, next) =>
-  res.status(200).send('Get auth user..');
+// IMPORT MODELS
+import User from '../models/User';
+
+// ROUTE            >     POST  /api/users/auth
+// DESC             >     AUTHENTICATE USER
+// ACCESS CONTROL   >     PUBLIC
+export const authenticateUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json(errors);
+
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username }).exec();
+
+    if (!user) return res.status(400).send('No user found!');
+
+    const isMatch = await compare(password, user.password);
+
+    if (!isMatch) return res.status(400).send('Password does not match!');
+
+    const token = await sign({ id: user.id }, process.env.JWT_KEY, {
+      expiresIn: '360000',
+    });
+
+    return res.status(200).json({
+      token,
+      user,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send('Something went wrong!');
+  }
+};
+
+// ROUTE            >     GET  /api/users/auth/user
+// DESC             >     GET AUTHENTICATED USER
+// ACCESS CONTROL   >     PRIVATE
+export const getAuthUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id).exec();
+
+    if (!user) return res.status(401).send('No user, Not authenticated!');
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send('Something went wrong!');
+  }
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,33 @@
 import { Router } from 'express';
+import { check } from 'express-validator';
 
 // IMPORT CONTROLLER
 import { authenticateUser, getAuthUser } from '../../controllers/auth';
 
+// IMPORT AUTH MIDDLEWARE
+import isAuth from '../../middleware/auth';
+
 const router = Router({ strict: true });
 
-router.post('/', authenticateUser);
-router.get('/user', getAuthUser);
+// ROUTE            >     POST  /api/users/auth
+// DESC             >     AUTHENTICATE USER
+// ACCESS CONTROL   >     PUBLIC
+router.post(
+  '/',
+  [
+    check('username', 'Please enter username!')
+      .not()
+      .isEmpty(),
+    check('password', 'Please enter password!')
+      .not()
+      .isEmpty(),
+  ],
+  authenticateUser
+);
+
+// ROUTE            >     GET  /api/users/auth/user
+// DESC             >     GET AUTHENTICATED USER
+// ACCESS CONTROL   >     PRIVATE
+router.get('/user', isAuth, getAuthUser);
 
 export default router;
