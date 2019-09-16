42401246ffbe2c6fe295a68456575178cf8505e6
rooneyrulz <roony0782045336@gmail.com>
2019-09-16 13:08:20 +0530

    established database connection

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { createServer } from 'http';
 import { config } from 'dotenv';
 import logger from 'morgan';
+import mongoose from 'mongoose';
+import bluebird from 'bluebird';
 
 // IMPORT ROUTES
 import testRoute from './routes/api/test';
@@ -20,6 +22,28 @@ app.use(logger('dev'));
 app.use('/api/test', testRoute);
 app.use('/api/users', userRoute);
 app.use('/api/users/auth', authRoute);
+
+// USE MONGOOSE PROMISE LIBRARY -BLUEBIRD
+mongoose.Promise = bluebird;
+
+// CONNECTING TO DATABASE
+async function init() {
+  try {
+    const isConnected = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+
+    if (isConnected) console.log('connecting to mongodb...');
+  } catch (error) {
+    process.exit(1);
+    console.log(error.message);
+  }
+}
+
+// ESTABLISH MONGO CONNECTION
+init();
 
 // LISTEN TO THE SERVER
 server.listen(process.env.PORT || 5000, () =>
